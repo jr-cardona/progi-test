dadb983c82317869f77a162de815531fc59f30e7
Juan Cardona <jr.cardona.az@gmail.com>
2023-02-18 02:14:23 -0500

    feat: configure phpunit and perform first test

---
+++ tests/Unit/VehiclePriceCalculatorTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class VehiclePriceCalculatorTest extends TestCase
+{
+    public function testOk()
+    {
+        $this->assertTrue(true);
+    }
+}
